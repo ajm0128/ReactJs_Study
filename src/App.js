c596e75bd10bc9fafe6917441266d8adb198d041
ajm0128 <zlzl9879@gmail.com>
2018-03-08 14:07:47 -0800

    movie_app finish

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React, { Component } from 'react';
-import './App.css';
-import Movie from './Movie';
+import React, { Component } from "react";
+import "./App.css";
+import Movie from "./Movie";
 
 class App extends Component {
+  // Render: componentWillMount() -> render() -> componentDidMount()
+  // Update componentWillReceiveProps() -> shouldComponentUpdate() -> componentWillUpdate() -> render() -> componentDidUpdate()
 
-  state = {}
+  state = {};
 
-  componentDidMount(){
+  componentDidMount() {
     this._getMovies();
   }
 
@@ -26,23 +28,26 @@ class App extends Component {
   };
 
   _getMovies = async () => {
-    const movies = await this._callApi()
+    const movies = await this._callApi();
     this.setState({
-      movies  
-    })
-  }
+      movies
+    });
+  };
 
   _callApi = () => {
-    return fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating')
-    .then(response => response.json())
-    .then(json => json.data.movies)
-    .catch(err => console.log(err))
-  }
+    return fetch(
+      "https://yts.am/api/v2/list_movies.json?sort_by=rating"
+    )
+      .then(potato => potato.json())
+      .then(json => json.data.movies)
+      .catch(err => console.log(err));
+  };
 
   render() {
+    const { movies } = this.state;
     return (
-      <div className="App">
-        {this.state.movies ? this._renderMovies() : 'Loading'}
+      <div className={movies ? "App" : "App--loading"}>
+        {movies ? this._renderMovies() : "Loading"}
       </div>
     );
   }
